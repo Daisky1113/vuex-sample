d38cc22d9911a1a30551cbbd7b01130d562b664d
Daisky <nakamura.daisky@gmail.com>
2021-02-02 13:44:16 +0900

    画像の登録処理を実装

--- store/storage.js
+++ store/storage.js
@@ -2,5 +2,17 @@ import firebase from '~/plugins/firebase'
 
 export const state = () => ({})
 export const mutations = {}
-export const actions = {}
+export const actions = {
+  async avatarUpload({ commit }, payload) {
+    console.log(payload.file.name)
+    const imageRef = firebase.storage()
+      .ref()
+      .child(`avatar/${payload.uid}/${payload.file.name}`)
+
+    await imageRef.put(payload.file)
+    const url = await imageRef.getDownloadURL()
+
+    return url
+  }
+}
 export const getters = {}
