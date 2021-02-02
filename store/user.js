5df11e1e0070fe4d645a06ecfc526c234acceff9
Daisky <nakamura.daisky@gmail.com>
2021-02-02 14:05:16 +0900

    ユーザー情報登録用のstoreを定義

--- store/user.js
+++ store/user.js
@@ -1,6 +1,29 @@
 import firebase from '~/plugins/firebase'
 
-export const state = () => ({})
-export const mutations = {}
-export const actions = {}
+export const state = () => ({
+  userName: '',
+  userPhoto: ''
+})
+export const mutations = {
+  setUserName(state, userName) {
+    state.userName = userName
+  },
+  clearUserName(state) {
+    state.userName = ''
+  },
+  setUserPhoto(state, userPhoto) {
+    state.userPhoto = userPhoto
+  },
+  clearUserPhoto(state) {
+    state.userPhoto = ''
+  }
+}
+export const actions = {
+  async createuser({ commit }, payload) {
+    const { uid, ...data } = payload
+    await firebase.firestore().doc(`users/${uid}`).set(data)
+    commit('setUserName', data.userName)
+    commit('setUserPhoto', data.photoUrl)
+  }
+}
 export const getters = {}
