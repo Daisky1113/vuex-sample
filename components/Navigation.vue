de08bc3229267b336de1e1acc19cc9f96c7a8097
Daisky <nakamura.daisky@gmail.com>
2021-02-02 12:26:10 +0900

    全体で使うナビゲーションを作成

---
+++ components/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar fixed app>
+    <v-app-bar-title>Frontend3</v-app-bar-title>
+    <v-spacer />
+    <v-btn to="/login" text> login </v-btn>
+    <v-btn to="/signup" text outlined> signup </v-btn>
+    <v-btn text @click="logout"> ログアウト </v-btn>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  data: () => ({}),
+  computed: {},
+  methods: {
+    logout() {
+      console.log("logout");
+    },
+  },
+};
+</script>
