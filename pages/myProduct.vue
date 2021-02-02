fb5f8fcb732812bab176a7b6879c6687b68ec695
Daisky <nakamura.daisky@gmail.com>
2021-02-02 14:33:08 +0900

    プロダクト情報登録ページを定義

---
+++ pages/myProduct.vue
@@ -0,0 +1,126 @@
+<template>
+  <v-row justify="center">
+    <v-col cols="9" class="mt-3">
+      <v-card flat>
+        <v-row>
+          <v-col cols="3" class="mt-4">
+            <div class="d-flex justify-center mb-4">
+              <v-avatar size="128" class="mb-4">
+                <!-- <v-img :src="photoUrl" /> -->
+              </v-avatar>
+            </div>
+            <div class="d-flex justify-center">
+              <slot name="voteBtn" />
+            </div>
+          </v-col>
+          <v-col cols="9">
+            <v-card-text>
+              <p class="subtitle-2 font-weight-bold">プロダクト名</p>
+              <p class="text-body-1 mb-10">
+                {{ productName }}
+              </p>
+              <p class="subtitle-2 font-weight-bold">使用技術</p>
+              <div class="mb-10">
+                <v-chip
+                  v-for="(topic, index) in topicArr"
+                  :key="index + topic"
+                  class="ml-2"
+                >
+                  {{ topic }}
+                </v-chip>
+              </div>
+              <p class="subtitle-2 font-weight-bold">技術的工夫</p>
+              <div class="mb-10">
+                <p
+                  v-for="(p, index) in tecDetailArr"
+                  :key="index + p"
+                  class="text-body-1 mb-1"
+                >
+                  {{ p }}
+                </p>
+              </div>
+
+              <p class="subtitle-1 font-weight-bold">サービス的工夫</p>
+              <div class="mb-10">
+                <p
+                  v-for="(p, index) in serviceDetailArr"
+                  :key="index + p"
+                  class="text-body-1 mb-1"
+                >
+                  {{ p }}
+                </p>
+              </div>
+            </v-card-text>
+            <v-card-actions> </v-card-actions>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+    <v-col cols="3">
+      <v-card>
+        <v-card-title> プロダクト情報登録 </v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="productName"
+            outlined
+            dense
+            label="Product Name"
+          />
+          <v-text-field
+            v-model="tecTopics"
+            outlined
+            dense
+            label="使用技術(半角スペース区切り)"
+          />
+          <v-textarea
+            v-model="tecDetail"
+            outlined
+            dense
+            label="技術的こだわり"
+          />
+          <v-textarea
+            v-model="serviceDetail"
+            outlined
+            label="サービス的こだわり"
+          />
+        </v-card-text>
+        <v-card-actions class="pb-4">
+          <v-spacer />
+          <v-btn outlined small @click="registerProduct"> 登録する </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+<script>
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
+
+export default {
+  components: {},
+  data: () => ({
+    productName: "",
+    tecTopics: "",
+    tecDetail: "",
+    serviceDetail: "",
+  }),
+  computed: {
+    topicArr() {
+      return this.tecTopics.split(" ");
+    },
+    tecDetailArr() {
+      return this.tecDetail.split(/\r\n|\r/);
+    },
+    serviceDetailArr() {
+      return this.serviceDetail.split(/\r\n|\r/);
+    },
+  },
+  methods: {
+    registerProduct() {
+      console.log(this.productName);
+      console.log(this.tecTopic);
+      console.log(this.tecDetail);
+      console.log(this.serviceDetail);
+    },
+  },
+};
+</script>
