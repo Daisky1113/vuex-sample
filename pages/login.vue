8b45d0103f18df8db9b4caa6a5419094a75f6547
Daisky <nakamura.daisky@gmail.com>
2021-02-02 12:16:43 +0900

    ログイン画面を作成する

---
+++ pages/login.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-row class="login-wrapper" justify="center" align="center">
+    <v-col cols="12" md="6">
+      <v-card>
+        <v-card-title>ログイン</v-card-title>
+        <v-card-text>
+          <v-text-field
+            v-model="email"
+            outlined
+            dense
+            label="Email"
+            prepend-icon="mdi-email-outline"
+          />
+          <v-text-field
+            v-model="password"
+            outlined
+            dense
+            label="Password"
+            type="password"
+            prepend-icon="mdi-lock-outline"
+          />
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn outlined rounded small @click="login"> Login </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+<script>
+export default {
+  data: () => ({
+    email: "",
+    password: "",
+    loginLoading: false,
+  }),
+
+  methods: {
+    async login() {
+      console.log(this.email);
+      console.log(this.password);
+    },
+  },
+};
+</script>
+<style scoped>
+.login-wrapper {
+  height: 100vh;
+}
+</style>
