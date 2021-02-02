723e98ff59718a99ea49180cae7538c77f30bf75
Daisky <nakamura.daisky@gmail.com>
2021-02-02 12:31:20 +0900

    stateとmutationsを定義
    ありとあらゆるfirebaseのサービスで使うuidをstateで管理する。また、stateを変更するためのmutationも定義する

--- store/auth.js
+++ store/auth.js
@@ -1,6 +1,15 @@
 import firebase from '~/plugins/firebase'
 
-export const state = () => ({})
-export const mutations = {}
+export const state = () => ({
+  uid: null,
+})
+export const mutations = {
+  setUid(state, uid) {
+    state.uid = uid
+  },
+  clearUid(state) {
+    state.uid = null
+  }
+}
 export const actions = {}
 export const getters = {}
